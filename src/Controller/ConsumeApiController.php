c37fb5dbf5c9703ae89a774b620e857ca79ae399
ydanielaosorio <daniela.osorio@imexhs.com>
2022-05-31 10:56:15 -0500

    lista worklist

--- src/Controller/ConsumeApiController.php
+++ src/Controller/ConsumeApiController.php
@@ -2,19 +2,30 @@
 
 namespace App\Controller;
 
+use App\Services\GetStudies;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConsumeApiController extends AbstractController
 {
     /**
-     * @Route("/consume/api", name="app_consume_api")
+     * @Route("/workList", name="app_consume_api")
      */
-    public function index(): Response
+    public function index(GetStudies $studies, Request $request): Response
     {
+        $method = 'GET';
+        $domain = 'https://127.0.0.1:8000/gateway/' . 'pacs';
+
+        $studyList = $studies->getRequest($method, $domain);
+
+      
         return $this->render('consume_api/index.html.twig', [
-            'controller_name' => 'ConsumeApiController',
+            'studyList' => $studyList,
         ]);
     }
+
+    
 }
