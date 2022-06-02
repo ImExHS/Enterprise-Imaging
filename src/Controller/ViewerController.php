7d9585a53672b0e071c18a0592ec6da6fde9af14
ydanielaosorio <daniela.osorio@imexhs.com>
2022-06-02 14:00:15 -0500

    Se agrega visor

--- src/Controller/ViewerController.php
+++ src/Controller/ViewerController.php
@@ -3,18 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class ViewerController extends AbstractController
 {
     /**
      * @Route("/viewer/daniela", name="app_viewer")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('viewer/index.html.twig', [
-            'controller_name' => 'ViewerController',
-        ]);
+        $idStudy = $request->query->get('idStudy');
+        return $this->redirect('http://143.198.25.142:8015/viewer/' . $idStudy);
     }
 }
